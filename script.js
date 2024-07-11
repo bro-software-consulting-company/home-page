689cc19477872c04a0453bec969b35def8334706
Cristovao Olegario de Castro <cristovaoolegario@gmail.com>
2024-07-10 23:55:06 -0300

    chore: remove old version

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mensagem enviada com sucesso!');
-    this.reset();
-});
